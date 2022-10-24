f6fd36959c84e7027964adf0dbf1303d18a34776
Najee Reno <nereno96@gmail.com>
2022-10-24 10:03:58 -0400

    checkpoint working mortgage calculator

--- scripts/mortgage.js
+++ scripts/mortgage.js
@@ -13,12 +13,27 @@ function onCalculateBtnClicked()
     const principalField = document.getElementById("principalField");
     let num1 = Number(principalField.value);
     const interestField = document.getElementById("interestField");
-    let num2 = Number(interestField.value);
+    let num2 = Number(interestField.value) / 100;
     const loanLengthField = document.getElementById("loanLengthField");
     let num3 = Number(loanLengthField.value);
 
-    let ans = num1 * (num2 / (1 - (1 + (num2 / 100)) ** - num3));
+
+
+    let p = num1;
+    let r = (num2) / 12;
+    let n = num3 * 12;
+
+    console.log(p);
+    console.log(r);
+    console.log(n);
+
+
+    let ans = p * (r * Math.pow((1 + r), n) / (Math.pow((1 + r), n) - 1));
+
+    let ans2 = (ans * n) - p;
 
     const paymentField = document.getElementById("paymentField");
-    paymentField.value = ans;
+    paymentField.value = ans.toFixed(2);
+    const totalInterestField = document.getElementById("totalInterestField");
+    totalInterestField.value = ans2.toFixed(2);
 }
